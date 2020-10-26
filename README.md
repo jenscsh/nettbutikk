d7494fe1285b21b420ebfbb33afdb670d2a45003
jenscsh <jenschrsh@gmail.com>
2020-10-26 04:30:47 +0100

    Ferdig til innlevering

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# nettbutikk
+Det har skjedd en del i det siste som gjør at jeg ikke fikk jobbet så mye med oppgaven som jeg skulle.
+Derfor blir den levert litt på etterskudd, håper det går greit denne gangen.
+
+Nettbutikken har seks forskjellige produkter med navn, pris, kategori og bildelink, som er lagret i en array. Ved å trykke på "kjøp"
+kan man legge til varer i handlekurven, som man også kan få frem ved å trykke på handlekurv-ikonet. Det er ikke mulig å legge til
+mer enn en av samme vare.
+I tillegg til å legges til som et element, blir også varen lagt til i en array for å kunne regne ut pris. Når man
+fjerner en vare fra handlekurven vil man også fjerne den fra arrayen. I tillegg har butikken også mulighet
+til å filtrere varer basert på kategori. Flere filtere kan velges på en gang. Hvis ingen er valgt vil ingen av varene filtreres.
+Selve varevisningen er en flexbox, mens filterne er presentert i en grid. Selve handlekurvvarene viser innholdet sitt ved hjelp av flex.
+
+Når det kommer til universell utforming har jeg eksperimentert med em, rem, % vh, vw osv. Jeg har også lagt til slik at hvis skjermen
+er mindre enn 900px, vil filtermenyen gjemme seg til siden, så kan man få den fram igjen ved å trykke på "Vis filter"-knappen,
+som kommer frem da. Siden er inspirert av andre nettbutikker for elektronikk, som komplett.no og elkjop.no.
+
+Av funksjoner som ble vurdert, men ikke lagt til i denne omgang, vurderte jeg egne produktsider og at man kan legge til mer enn en vare om gangen
+og å ha en egen kasseside.
--- index.js
+++ index.js
@@ -1,49 +1,58 @@
+//Henter html-elementer.
 const WAREOVERVIEW = document.querySelector(".shop");
 const CARTVIEW = document.querySelector(".cartbox");
 const CARTVIEWLIST =document.querySelector(".cartboxlist");
 const TOTALPRICEVIEW = document.querySelector(".totalprice");
 const FILTEROPTIONS =document.querySelectorAll(".filteroption");
+const SIDEBAR = document.querySelector(".sidebar");
+const FILTERBTNS = document.querySelectorAll(".filterbutton");
+//De tilgjengelige varene.
 const WARES = [
     {
         name: "Skjerm",
         price: 2099,
         category: "Datautstyr",
-        picture: "./images/placeholder.png"
+        picture: "./images/skjerm.png"
     },
     {
         name: "Vaskemaskin",
         price: 5199,
         category: "Hvitevarer",
-        picture: "./images/placeholder.png"
+        picture: "./images/vaskemaskin.png"
     },
     {
         name: "Mikrobølgeovn",
         price: 899,
         category: "Hvitevarer",
-        picture: "./images/placeholder.png"
+        picture: "./images/mikroovn.webp"
     },
     {
         name: "Smarttelefon",
         price: 6190,
         category: "Telefon og nettbrett",
-        picture: "./images/placeholder.png"
+        picture: "./images/smartphone.png"
     },
     {
         name: "TV",
         price: 8790,
         category: "Lyd og bilde",
-        picture: "./images/placeholder.png"
+        picture: "./images/tv.webp"
     },
     {
         name: "Høytaler",
         price: 1990,
         category: "Lyd og bilde",
-        picture: "./images/placeholder.png"
+        picture: "./images/speaker.webp"
     }
 ]
+
+//Handlevognen
 let cart = [];
 
-WARES.forEach(item => {
+//Funksjon for å standard varelisting uten filter. Kjører når siden blir lastet inn.
+DefaultFill();
+function DefaultFill() {
+    WARES.forEach(item => {
     WAREOVERVIEW.innerHTML+= `
     <div class="wareitem">
         <img class="wareimage" src="${item.picture}" alt="">
@@ -52,16 +61,33 @@ WARES.forEach(item => {
         <button class="addtocart" onclick="AddToCart(${WARES.indexOf(item)})">Kjøp</button>
     </div>
     `
-});
+    });
+}
+
+//Funksjon for å filtrere varer. Kjører når noen trykker på filterboksene.
 function FilterWares () {
+    //Tømmer eksisterende varelisting. Legger til varer til array hvis kategori passer.
     WAREOVERVIEW.innerHTML="";
-    let waresToAdd = [];
+    var waresToAdd = [];
     FILTEROPTIONS.forEach(filter=> {
         if (filter.checked==true) {
-            waresToAdd+=WARES.filter(category === filter.value);
+            waresToAdd.push(...WARES.filter(item=>item.category==filter.value));
         }
-    })
+    });
+    //Legger til filtrerte varer til vareoversikt. Hvis det er ingen kjører standard varelisting.
+    waresToAdd.forEach(ware => {
+        WAREOVERVIEW.innerHTML+= `
+        <div class="wareitem">
+            <img class="wareimage" src="${ware.picture}" alt="">
+            <h2 class="warename">${ware.name}</h2>
+            <h3 class="wareprice">${ware.price},-</h3>
+            <button class="addtocart" onclick="AddToCart(${WARES.indexOf(ware)})">Kjøp</button>
+        </div>
+        `
+    });
+    if (waresToAdd.length===0) DefaultFill();
 }
+//Åpner/lukker handlekurv.
 function OpenCloseCart() {
     if(CARTVIEW.style.display==='none') {
         CARTVIEW.style.display='block';
@@ -69,36 +95,54 @@ function OpenCloseCart() {
         CARTVIEW.style.display='none';
     }
 }
+//Legger til vareoversikt. Foreløpig går det ikke an å legge til flere varer av samme type.
 function AddToCart(id) {
     if (cart.includes(WARES[id])) {
-        console.log("Already exist");
+        console.log("Already exist: "+cart);
     } else {
         cart.push(WARES[id]);
         let addedItem =`
         <div class="cartitem" id="cartItem${id}">
-            <img class="cartitemimage" src="${WARES[id].picture}">
             <h2 class="cartitemname">${WARES[id].name}</h2>
+            <img class="cartitemimage" src="${WARES[id].picture}">
             <h3 class="cartitemprice">${WARES[id].price},-</h3>
             <button class="removefromcart" onclick="RemoveFromCart(${id})">Fjern</button>
         </div>
         `;
-    CARTVIEWLIST.innerHTML+= addedItem;
+        CARTVIEWLIST.innerHTML+= addedItem;
+        if (CARTVIEW.style.display==='none') OpenCloseCart();
     }
     UpdateTotalPrice();
 }
+//Fjerner varer fra handlevogn.
 function RemoveFromCart(toRemove) {
-    cart.pop(WARES[toRemove]);
+    cart.splice(cart.indexOf(WARES[toRemove]),1);
     CARTVIEWLIST.removeChild(document.getElementById("cartItem"+toRemove));
     UpdateTotalPrice();
 }
+//Oppdaterer totalpris. Hvis ingen varer er tilstede, vises standardtekst.
 function UpdateTotalPrice () {
     if (cart.length===0) {
         TOTALPRICEVIEW.textContent="Her er det foreløpig ingenting.";
+        OpenCloseCart();
     } else {
         let total=0;
         cart.forEach(item=> {
             total+=item.price;
         })
-        TOTALPRICEVIEW.textContent=`Total pris: ${total},-`;
+        TOTALPRICEVIEW.innerHTML=`Total pris: ${total},-`.bold();
     }
+}
+//Åpner og lukker filtermeny når den er i mobilmodus. Knapper for å kontrollere dette kan bare sees da.
+var open=false;
+function OpenCloseFilters() {
+    if (open) {
+        SIDEBAR.style.left='-100vw';
+        FILTERBTNS.forEach(btn => {btn.innerHTML="Vis filter".bold()});
+    }
+    else {
+        SIDEBAR.style.left='0vw';
+        FILTERBTNS.forEach(btn => {btn.innerHTML="Skjul filter".bold()});
+    }
+    open=!open;
 }
